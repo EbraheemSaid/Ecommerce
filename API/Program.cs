2d0fed2c65747f91d13a09f0134f06ccb232c8ac
Ebraheem <ebraheemsaid7@gmail.com>
2025-06-29 05:09:03 +0300

    Add project files.

---
+++ API/Program.cs
@@ -0,0 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<Infrastructure.Data.StoreContext>(opt =>
+{
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.MapControllers();
+
+app.Run();
