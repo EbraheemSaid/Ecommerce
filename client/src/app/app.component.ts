3b471178409df11a0de943e893be6bfc23308c02
Ebraheem <ebraheemsaid7@gmail.com>
2025-07-06 16:31:04 +0300

    implementing the UI with search, pagination, filters features

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -4,32 +4,16 @@ import { HeaderComponent } from './layout/header/header.component';
 import { HttpClient } from '@angular/common/http';
 import { Product } from './shared/models/product';
 import { Pagination } from './shared/models/pagination';
+import { ShopService } from './core/services/shop.service';
+import { ShopComponent } from "./features/shop/shop.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, ShopComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent implements OnInit {
-  baseUrl = 'https://localhost:5001/api/';
-  private http = inject(HttpClient);
-  title = 'client';
-  products: Product[] = [];
-
-  ngOnInit() {
-    this.http.get<Pagination<Product>>(this.baseUrl + 'products').subscribe({
-      next: (response) => {
-        this.products = response.data;
-        console.log(response);
-      },
-      error: (error) => {
-        console.error(error);
-      },
-      complete: () => {
-        console.log('Request completed');
-      },
-    });
-  }
+export class AppComponent {
+  title = 'Ecommerce';
 }
