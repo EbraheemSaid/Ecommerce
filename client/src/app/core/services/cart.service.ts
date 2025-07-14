d88d6eabe5f9637bb2663de3ffdf53b122954488
Ebraheem <ebraheemsaid7@gmail.com>
2025-07-14 22:40:08 +0300

    Add initial splash screen and enhance cart service with item count computation

--- client/src/app/core/services/cart.service.ts
+++ client/src/app/core/services/cart.service.ts
@@ -1,8 +1,9 @@
-import { inject, Injectable, signal } from '@angular/core';
+import { computed, inject, Injectable, signal } from '@angular/core';
 import { environment } from '../../../environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { Cart, CartItem } from '../../shared/models/cart';
 import { Product } from '../../shared/models/product';
+import { map, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -11,11 +12,17 @@ export class CartService {
   baseUrl = environment.apiUrl;
   private http = inject(HttpClient);
   cart = signal<Cart | null>(null);
+  itemCount = computed(
+    () => this.cart()?.items.reduce((sum, item) => sum + item.quantity, 0) ?? 0
+  );
 
   getCart(id: string) {
-    return this.http.get<Cart>(this.baseUrl + 'cart?id=' + id).subscribe({
-      next: (cart) => this.cart.set(cart),
-    });
+    return this.http.get<Cart>(this.baseUrl + 'cart?id=' + id).pipe(
+      map((cart) => {
+        this.cart.set(cart);
+        return cart;
+      })
+    );
   }
 
   setCart(cart: Cart) {
@@ -38,7 +45,7 @@ export class CartService {
     item: CartItem,
     quantity: number
   ): CartItem[] {
-    const index = items.findIndex((x) => (x.productId = item.productId));
+    const index = items.findIndex((x) => x.productId === item.productId);
     if (index === -1) {
       item.quantity = quantity;
       items.push(item);
